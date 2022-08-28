41e1130f1fcb288840bac7a56a03ad6e152878e8
CPuPush <fpakpahan44@gmail.com>
2022-08-28 23:57:08 +0700

    FINISHING FS

--- Session2-EventHandlerNodeJSBufferStreams/FileSystemNode/deleteFs.js
+++ Session2-EventHandlerNodeJSBufferStreams/FileSystemNode/deleteFs.js
@@ -1,3 +1,12 @@
 /*
-modul fs memiliki fungsi unlink() untuk menghapus file. Fungsi ini memiliki unlink() untuk menghapus pesan
+modul fs memiliki fungsi unlink() untuk menghapus file. Fungsi memiliki 2 parameter
+1. nama fila yang akan dihapus
+2. fungsi yang akan dieksekusi saat file dihapus
 */
+
+const fs = require('fs');
+
+fs.unlink('rubahnama.txt', (err) => {
+  if(err) throw err;
+  console.log('File had been deleted');
+});
