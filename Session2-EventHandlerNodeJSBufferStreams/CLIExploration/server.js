d98b44520effda504e1f688e80f120177e329138
CPuPush <fpakpahan44@gmail.com>
2022-08-29 00:10:30 +0700

    basic cli

---
+++ Session2-EventHandlerNodeJSBufferStreams/CLIExploration/server.js
@@ -0,0 +1,10 @@
+/*
+Latihan menampilkan data menggunakan CommandLine
+Teknologi 
+1. Nodejs javascript on server side
+2. commander.js command-line interfaces made easy
+3. cli-table pretty unicode tables for the CLI with Nodejs
+4. axios, promise based HTTP client for the browser and nodejs 
+5. moment.js parse, validate, manipulate, and display dates and times in js
+
+*/
